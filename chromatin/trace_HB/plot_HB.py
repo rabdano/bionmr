a63c4442bc85fb07bc60198fde0caf36941e5af7
Sevastyan Rabdano <sevastyan@rabdano.ru>
2019-05-07 14:23:00 +0300

    Add scripts for calculation and plotting HB traces (xmol_traj)

---
+++ chromatin/trace_HB/plot_HB.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from matplotlib.backends.backend_pdf import PdfPages
+
+# User input:
+hb_trace = 'hb_trace.dat'  # file with data about hydrogen bonds
+avg_win = 100  # window for averaging of data
+step = 25  # traces per page in figures
+rid_of_interest = list(range(136, 160)) + list(range(623, 647))
+
+def split_header(header):
+    # THR~3::N--DC~1133::OP1 THR~3::OG1--DC~1133::OP1
+    result = []
+    header = header.rstrip('\n').strip()
+    hbs = header.split(' ')
+    for hb in hbs:
+        donor, acceptor = tuple(hb.split('--'))
+        donor_r = donor.split('::')[0]
+        donor_aName = donor.split('::')[1]
+        donor_rName = donor_r.split('~')[0]
+        donor_rId = int(donor_r.split('~')[1])
+        acceptor_r = acceptor.split('::')[0]
+        acceptor_aName = acceptor.split('::')[1]
+        acceptor_rName = acceptor_r.split('~')[0]
+        acceptor_rId = int(acceptor_r.split('~')[1])
+        result.append([[donor_rName, donor_rId, donor_aName],
+                       [acceptor_rName, acceptor_rId, acceptor_aName]])
+    return result
+
+
+def average_data(data, window=100):
+    # average data over windows
+    # e.g. if in window hydrogen bond is present > 50% then value for window is 1
+    n, m = data.shape
+    nw = int(n/window)
+    result = np.zeros((nw, m))
+    for start in range(0, len(data), window):
+        result[int(start/window), :] = np.around(np.mean(data[start:start+window,:], axis=0))
+    return result
+
+
+# read data and parameters
+with open(hb_trace, 'r') as f:
+    header = f.readline()
+data = np.genfromtxt(hb_trace, skip_header=1)
+with open('input.json', 'r') as f:
+    pars = json.load(f)
+trj_filename_first = pars['trj_filename_first']
+trj_filename_last = pars['trj_filename_last']
+stride = pars['stride']
+
+
+# extract header and average data
+hbs = split_header(header)
+avg_data = average_data(data, avg_win)
+
+
+# plot figures for all HBs
+
+cmap = plt.get_cmap('binary', 2)
+
+with PdfPages('HB_figures.pdf') as pdf:
+    for hb_idx in range(0, len(hbs), step):
+        plt.figure(figsize=(8, 8), dpi=96)
+
+        # get indexes
+        i1 = hb_idx
+        i2 = hb_idx + step
+        if i2 >= len(hbs):
+            i2 = len(hbs)
+
+        # get data
+        cur_data = avg_data[:, i1:i2]
+
+        # plot image
+        ax = plt.gca()
+        im = ax.imshow(cur_data.T, cmap=cmap, aspect='auto')
+
+        # setup ticks and labels
+        plt.yticks(range(step), ['%s' % hbs[i] for i in range(step)])
+        plt.xlabel('Time, ns')
+
+        # save figure
+        pdf.savefig(bbox_inches='tight')
+        plt.close()
+
+
+# plot figures for H4 only
+
+# prune data for not H4 tails
+to_del = []
+for i, hb in enumerate(hbs):
+    if not (hb[0][1] in rid_of_interest) and not (hb[1][1] in rid_of_interest):
+        to_del.append(i)
+hbs = [hb for i, hb in enumerate(hbs) if i not in to_del]
+avg_data = np.delete(avg_data, to_del, axis=1)
+
+with PdfPages('HB_figures_H4.pdf') as pdf:
+    plt.figure(figsize=(8, 8), dpi=96)
+
+    # get data
+    cur_data = avg_data
+
+    # setup ticks and labels
+    plt.yticks(range(len(hbs)), ['%s' % hbs[i] for i in range(len(hbs))])
+    plt.xlabel('Time, ns')
+
+    # plot image
+    ax = plt.gca()
+    im = ax.imshow(cur_data.T, cmap=cmap, aspect='auto')
+
+    # save figure
+    pdf.savefig(bbox_inches='tight')
+    plt.close()
