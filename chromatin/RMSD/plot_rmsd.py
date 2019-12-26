3edce28777135ea0c31735e0161ed9a112c7f1e0
Sevastyan Rabdano <sevastyan@rabdano.ru>
2019-12-26 15:00:26 +0300

    Implement RMSD calculation

--- chromatin/RMSD/plot_rmsd.py
+++ chromatin/RMSD/plot_rmsd.py
@@ -17,17 +17,20 @@ def cm2inch(*tupl):
         return tuple(i/inch for i in tupl)
 
 
-files = sorted(glob('rmsd_*.csv'))
+# files = sorted(glob('rmsd_*.csv'))
+files = ["rmsd_ref_align_ca_ss.csv", "rmsd_ref_align_dna.csv"]
+lab = ["sec.str. CA", "DNA N1, N9"]
 plt.figure(figsize=cm2inch(16, 12), dpi=300)
 
 for i, fn in enumerate(files):
     data = np.loadtxt(fn, skiprows=1, delimiter=",")
     x = data[:, 0] / 1000
     y = data[:, 1]
-    plt.plot(x, y, color=colors[i], linewidth=1.0)
+    plt.plot(x, y, color=colors[i], linewidth=1.0, label=lab[i])
 
 plt.xlabel("Time, ns")
 plt.ylabel(r"RMSD, ${\rm\AA}$")
+plt.legend()
 
 plt.savefig("RMSD.png", bbox_inches="tight")
 plt.savefig("RMSD.pdf", bbox_inches="tight")
