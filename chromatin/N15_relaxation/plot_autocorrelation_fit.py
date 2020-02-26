f6436536b00355c3cbe50f7de54a63bcf05e1b9a
Sevastyan Rabdano <sevastyan@rabdano.ru>
2020-02-26 19:23:05 +0300

    Update script for plotting of ACFs and fits

--- chromatin/N15_relaxation/plot_autocorrelation_fit.py
+++ chromatin/N15_relaxation/plot_autocorrelation_fit.py
@@ -36,6 +36,10 @@ for file, a in zip(files, acfs):
     # get rId and rName
     rId = int(a.split()[0].split('-')[0])
     rName = a.split()[0].split('-')[1]
+    if rId < 300:
+        rId = rId - 135
+    else:
+        rId = rId - 622
 
     # get amps and taus
     amps = [float(x) for x in a.split()[1:1+n_exp]]
@@ -51,7 +55,24 @@ for file, a in zip(files, acfs):
     plt.plot(data[:, 0], data[:, 1], 'b-', linewidth=0.5)
     plt.plot(data[:, 0], data_fit, 'r-', linewidth=0.5)
     plt.xlabel('Time, ps')
-    axis_limits = [-10, data[-1, 0]+10, -0.2, 1.01]
+    # axis_limits = [-10, data[-1, 0]+10, -0.2, 1.01]
+    axis_limits = [-1000, 101000, 0, 1.01]
+    txt = "amp / tau[ns]\n"
+    i = 0
+    for a, t in zip(amps, taus):
+        i += 1
+        if i == len(amps):
+            txt += "{:.2f} / {:.3f}".format(a, t/1e3)
+        else:
+            txt += "{:.2f} / {:.3f}\n".format(a, t/1e3)
+    plt.text(x=0.7,
+             y=0.7,
+             s=txt,
+             horizontalalignment='left',
+             verticalalignment='top',
+             bbox=dict(facecolor='white'),
+             fontsize=12,
+             transform=plt.gca().transAxes)
     plt.axis(axis_limits)
     plt.legend(['%s%d data' % (rName, rId), '%s%d fit' % (rName, rId)])
     plt.grid(True)
