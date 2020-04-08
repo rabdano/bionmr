39ac183b8ebe191f33a67c838d3ae333687d1361
Seva <sevastyan@rabdano.ru>
2020-04-08 20:29:56 +0300

    RMSD and contacts for DNA

--- chromatin/DNA_RMSD_contacts/plot_DNA_RMSD_contacts.py
+++ chromatin/DNA_RMSD_contacts/plot_DNA_RMSD_contacts.py
@@ -8,13 +8,13 @@ with open("input.json", "r") as f:
 first_dat_file = int(pars["first_dat_file"])
 last_dat_file = int(pars["last_dat_file"])
 stride = int(pars["stride"])
-rmsf_fnout = pars["rmsf_fnout"]
+rmsd_fnout = pars["rmsd_fnout"]
 contacts_fnout = pars["contacts_fnout"]
 fig_fn = pars["fig_fn"]
 
 
 # load data
-position, rmsf = np.genfromtxt(rmsf_fnout,
+position, rmsd = np.genfromtxt(rmsd_fnout,
                                delimiter=",",
                                skip_header=1,
                                usecols=(0, 1),
@@ -31,8 +31,8 @@ axs[0].bar(position, contacts)
 axs[0].set(ylabel="Contacts", ylim=[0, np.max(contacts) * 1.05])
 axs[0].text(0.03, 0.9, "A", fontsize=12, ha="center", va="center", transform=axs[0].transAxes)
 axs[1].grid()
-axs[1].plot(position, rmsf, marker="o", ms=4, linewidth=0.5)
-axs[1].set(ylabel=r"RMSF, $\rm\AA$", ylim=[0, 5.999])
+axs[1].plot(position, rmsd, marker="o", ms=4, linewidth=0.5)
+axs[1].set(ylabel=r"RMSD, $\rm\AA$", ylim=[0, 5.999])
 axs[1].text(0.03, 0.9, "B", fontsize=12, ha="center", va="center", transform=axs[1].transAxes)
 for ax in axs:
     ax.set(xlabel="Position", xlim=[-73, 73], xticks=range(-70, 74, 10))
--- chromatin/DNA_RMSD_contacts/run.sh
+++ chromatin/DNA_RMSD_contacts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3.8 calc_rmsf.py
+python3.8 calc_rmsd.py
 python3.8 calc_contacts.py
 python3.8 plot_DNA_RMSD_contacts.py
