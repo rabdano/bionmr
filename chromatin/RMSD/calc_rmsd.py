d5f89a1d4b9f67da33feafd1262ba509e23f45c7
Seva <sevastyan@rabdano.ru>
2020-06-20 16:47:17 +0300

    RMSD calculation using alignment by protein sec.str. CA. Updated graphics.

--- chromatin/RMSD/calc_rmsd.py
+++ chromatin/RMSD/calc_rmsd.py
@@ -2,30 +2,124 @@ from bionmr_utils.md import *
 from get_secondary_structure_residues import *
 from tqdm import tqdm
 import numpy as np
-from timeit import default_timer as timer
+import json
 
 
-# setup trajectory parameters
-first_dat_file = 1
-last_dat_file = 1000
-stride = 1000  # ps
-# reference PDB
-structure = "1kx5"  # 1kx5 or 3lz0
-len_inner_turn = 39
-# probe atoms
-protein_and_dna_chains = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-probe = ((cName == 'I') & (aName == 'P'))
-dna_align_pred = ((aName == "N1") | (aName == "N9"))
-if structure == "1kx5":
-    reference_pdb = "/home/seva/chromatin/3_solution/pdb/amber/2_tleap/1kx5_propka_renumbered.pdb"
-if structure == "3lz0":
-    reference_pdb = "/home/seva/chromatin/5_solution_Widom_601/pdb/Amber/1_propka/01531-propka.pdb"
+#
+#  setup trajectory parameters
+#
+with open("input.json", "r") as f:
+    pars = json.load(f)
+first_dat_file = int(pars["first_dat_file"])
+last_dat_file = int(pars["last_dat_file"])
+stride = int(pars["stride"])
+reference_pdb = pars["reference_pdb"]
+rmsd_fnout = pars["rmsd_fnout"]
+len_inner_turn = pars["len_inner_turn"]
 
 
+#
+#  set atom selections
+#
+protein_and_dna_chains = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+probe = ((cName == "I") & (aName == "P"))
+dna_align_pred = aName.is_in({"N1", "N9"})
+
+
+#
+#  load trajectory
+#
+path_to_traj = "../.."
+traj, ref = traj_from_dir(path=path_to_traj,
+                          reference_pdb=path_to_traj + "/1_build/ref.pdb",
+                          stride=1,
+                          first=first_dat_file,
+                          last=last_dat_file)
+
+print("Trajectory contain %d frames with %d chains / %d residues / %d atoms" %
+      (len(traj), len(traj[0].asChains), len(traj[0].asResidues), len(traj[0].asAtoms)))
+print("Using run%05d.dat - run%05d.dat" % (first_dat_file, last_dat_file))
+
+
+#
+#  get secondary structure information:
+#  get residue ids for secondary structure from DSSP using Biopython and PDB
+#
+protein_chains = "ABCDEFGH"
+ss_residues_by_chain = []
+incr = 0
+for c in protein_chains:
+    ss = get_secondary_structure_residues(c, pdb_code="1KX5")
+    ss_residues_by_chain.append(set([(x+incr) for x in ss]))
+    incr += len(ref.asResidues.filter(cName == c))
+# print("Secondary structure residues by chain")
+# print(ss_residues_by_chain)
+ss_residues = [r for chain_r in ss_residues_by_chain for r in chain_r]
+# print("Secondary structure residues combined")
+# print(ss_residues)
+ss_residues = set(ss_residues)
+
+
+#
+#  get residue ids for inner and outer DNA turns
+#
+dna = ref.asAtoms.filter(aName == "N1")
+dna_resids = []
+for at in dna:
+    dna_resids.append(int(at.rId.serial))
+n_dna_resids = len(dna_resids)
+n_dna_bp = int(n_dna_resids / 2)
+idx_01 = int(dna_resids[0]-1 + n_dna_bp // 2)
+idx_02 = int(dna_resids[0]-1 + n_dna_bp + n_dna_bp // 2)
+inner_turn = set(list(range(idx_01-len_inner_turn, idx_01+len_inner_turn+1, 1)) +
+                 list(range(idx_02-len_inner_turn, idx_02+len_inner_turn+1, 1)))
+
+outer_turn = set(list(range(dna_resids[0], idx_01-len_inner_turn, 1)) +
+                 list(range(idx_01+len_inner_turn+1, dna_resids[0]+n_dna_bp+1, 1)) +
+                 list(range(dna_resids[0]+n_dna_bp+1, idx_02-len_inner_turn, 1)) +
+                 list(range(idx_02+len_inner_turn+1, dna_resids[-1]+1, 1)))
+print("Nucleotides:", n_dna_resids)
+print("Base pairs:", n_dna_bp)
+print("Middle index I:", idx_01)
+print("Middle index J:", idx_02)
+print("Inner turn:", idx_01-len_inner_turn, "-", idx_01+len_inner_turn, ",",
+                     idx_02-len_inner_turn, "-", idx_02+len_inner_turn)
+print("Outer turn:", dna_resids[0], "-", idx_01-len_inner_turn-1, ",",
+                     idx_01+len_inner_turn+1, "-", dna_resids[0]+n_dna_bp, ",",
+                     dna_resids[0]+n_dna_bp+1, "-", idx_02-len_inner_turn-1, ",",
+                     idx_02+len_inner_turn+1, "-", dna_resids[-1])
+
+
+#
+#  reference atoms for alignment
+#  use 3LZ0 structure with reconstructed tails and propka protonation
+#
+reference = PdbFile(reference_pdb).get_frame()
+ref_ats = reference.asAtoms
+ref_align_ca_ss = ref_ats.filter((aName == "CA") & (rId.is_in(ss_residues)))
+ref_align_dna = ref_ats.filter(dna_align_pred)
+ref_align_union = ref_ats.filter(((aName == "CA") & (rId.is_in(ss_residues))) | dna_align_pred)
+ref_align_inner_turn = ref_ats.filter(dna_align_pred).filter(rId.is_in(inner_turn))
+ref_align_outer_turn = ref_ats.filter(dna_align_pred).filter(rId.is_in(outer_turn))
+
+
+#
+#  create data variables
+#
+rmsd_ref_align_ca_ss = np.zeros(int(traj.size / stride))
+rmsd_ref_align_dna = np.zeros(int(traj.size / stride))
+rmsd_ref_align_union = np.zeros(int(traj.size / stride))
+rmsd_ref_align_inner_turn = np.zeros(int(traj.size / stride))
+rmsd_ref_align_outer_turn = np.zeros(int(traj.size / stride))
+
+
+#
+#  Handle PBC:
+#
 def get_XST(path_to_traj):
-    time, V = np.genfromtxt(path_to_traj + '/5_run/summary.VOLUME', unpack=True)
+    time, V = np.genfromtxt(path_to_traj + "/5_run/summary.VOLUME", unpack=True)
 
-    inpcrd = path_to_traj + '/1_build/box.inpcrd'
+    inpcrd = path_to_traj + "/1_build/box.inpcrd"
 
     with open(inpcrd) as f:
         content = f.readlines()
@@ -64,217 +158,89 @@ def get_XST(path_to_traj):
     return xst
 
 
-# load trajectory
-path_to_traj = "../.."
-traj, ref = traj_from_dir(path=path_to_traj,
-                          reference_pdb=path_to_traj + "/1_build/ref.pdb",
-                          stride=1,
-                          first=first_dat_file,
-                          last=last_dat_file)
-
-print("Trajectory contain %d frames with %d chains / %d residues / %d atoms" % (
-    len(traj), len(traj[0].asChains), len(traj[0].asResidues), len(traj[0].asAtoms)))
-print("Using run%05d.dat - run%05d.dat" % (first_dat_file, last_dat_file))
-
-
-# get residue ids for secondary structure from DSSP using Biopython and PDB
-protein_chains = 'ABCDEFGH'
-ss_residues_by_chain = []
-incr = 0
-for c in protein_chains:
-    ss = get_secondary_structure_residues(c, pdb_code='1KX5')
-    ss_residues_by_chain.append(set([(x+incr) for x in ss]))
-    incr += len(ref.asResidues.filter(cName == c))
-# print("Secondary structure residues by chain")
-# print(ss_residues_by_chain)
-ss_residues = [r for chain_r in ss_residues_by_chain for r in chain_r]
-# print("Secondary structure residues combined")
-# print(ss_residues)
-ss_residues = set(ss_residues)
-
-
-# get residue ids for inner and outer DNA turns (-39 to 39)
-dna = ref.asAtoms.filter(aName == "P")
-dna_resids = []
-for at in dna:
-    dna_resids.append(int(at.rId.serial))
-n_dna_resids = len(dna_resids)+2  # +2 because 5' ends are not phosphorylated and we count by P atoms
-n_dna_bp = int(n_dna_resids / 2)
-idx_01 = int(dna_resids[0]-1 + n_dna_bp // 2)
-idx_02 = int(dna_resids[0]-1 + n_dna_bp + n_dna_bp // 2)
-inner_turn = set(list(range(idx_01-len_inner_turn, idx_01+len_inner_turn+1, 1)) +
-                 list(range(idx_02-len_inner_turn, idx_02+len_inner_turn+1, 1)))
-
-outer_turn = set(list(range(dna_resids[0], idx_01-len_inner_turn, 1)) +
-                 list(range(idx_01+len_inner_turn+1, dna_resids[0]+n_dna_bp+1, 1)) +
-                 list(range(dna_resids[0]+n_dna_bp+1, idx_02-len_inner_turn, 1)) +
-                 list(range(idx_02+len_inner_turn+1, dna_resids[-1]+1, 1)))
-print("Nucleotides:", n_dna_resids)
-print("Base pairs:", n_dna_bp)
-print("Middle index I:", idx_01)
-print("Middle index J:", idx_02)
-print("Inner turn:", idx_01-len_inner_turn, "-", idx_01+len_inner_turn, ",",
-                     idx_02-len_inner_turn, "-", idx_02+len_inner_turn)
-print("Outer turn:", dna_resids[0], "-", idx_01-len_inner_turn-1, ",",
-                     idx_01+len_inner_turn+1, "-", dna_resids[0]+n_dna_bp, ",",
-                     dna_resids[0]+n_dna_bp+1, "-", idx_02-len_inner_turn-1, ",",
-                     idx_02+len_inner_turn+1, "-", dna_resids[-1])
-
-
-# reference atoms for alignment
-reference = PdbFile(reference_pdb).get_frame()
-ref_ats = reference.asAtoms
-ref_align_protein = reference.asAtoms.filter(cName.is_in({"A", "B", "C", "D", "E", "F", "G", "H"}))
-ref_align_ca = reference.asAtoms.filter(aName == "CA")
-ref_align_ca_ss = reference.asAtoms.filter((aName == "CA") & (rId.is_in(ss_residues)))
-ref_align_dna = reference.asAtoms.filter(dna_align_pred)
-ref_align_inner_turn = reference.asAtoms.filter(dna_align_pred).filter(rId.is_in(inner_turn))
-ref_align_outer_turn = reference.asAtoms.filter(dna_align_pred).filter(rId.is_in(outer_turn))
-ref_align_union = reference.asAtoms.filter(((aName == "CA") & (rId.is_in(ss_residues))) | dna_align_pred)
-
-rmsd_ref_align_protein = np.zeros(int(traj.size / stride))
-rmsd_ref_align_ca = np.zeros(int(traj.size / stride))
-rmsd_ref_align_ca_ss = np.zeros(int(traj.size / stride))
-rmsd_ref_align_dna = np.zeros(int(traj.size / stride))
-rmsd_ref_align_inner_turn = np.zeros(int(traj.size / stride))
-rmsd_ref_align_outer_turn = np.zeros(int(traj.size / stride))
-rmsd_ref_align_union = np.zeros(int(traj.size / stride))
-
-
 # get PBC from XST file and inpcrd file
 pbc = get_XST(path_to_traj)
 v1 = VectorXYZ.from_numpy(pbc[:, 1:4])
 v2 = VectorXYZ.from_numpy(pbc[:, 4:7])
 v3 = VectorXYZ.from_numpy(pbc[:, 7:10])
+assert len(v1) >= traj.size, "PBC data is not enough"
 scaling_factors = [v1[i].len()/v1[0].len() for i in range(len(v1))]
 lat_vec = LatticeVectors(v1[0], v2[0], v3[0])
 shift_finder = BestShiftFinder(lat_vec)
-inpcrd = path_to_traj + '/1_build/box.inpcrd'
+inpcrd = path_to_traj + "/1_build/box.inpcrd"
 with open(inpcrd) as f:
     content = f.readlines()
 content = content[-1].split()[:6]
 a, b, c, alpha, beta, gamma = [float(x) for x in content]
 
-t2 = timer()
-# run through trajectory
+
+#
+#  run through trajectory
+#
 for frame in tqdm(traj[::stride], disable=False):
     if frame.index == 0:
+        # create variables for selections from atoms of frame
+        ref_pbc_ats = ref.asAtoms
         frame_ats = frame.asAtoms
 
-        ref_probe = ref_ats.filter(probe)
+        ref_probe = ref_pbc_ats.filter(probe)
         frame_probe = frame_ats.filter(probe)
 
-        frame_align_protein = frame_ats.filter(cName.is_in({"A", "B", "C", "D", "E", "F", "G", "H"}))
-        frame_align_ca = frame_ats.filter(aName == "CA")
         frame_align_ca_ss = frame_ats.filter((aName == "CA") & (rId.is_in(ss_residues)))
         frame_align_dna = frame_ats.filter(dna_align_pred)
+        frame_align_union = frame_ats.filter(((aName == "CA") & (rId.is_in(ss_residues))) | dna_align_pred)
         frame_align_inner_turn = frame_ats.filter(dna_align_pred).filter(rId.is_in(inner_turn))
         frame_align_outer_turn = frame_ats.filter(dna_align_pred).filter(rId.is_in(outer_turn))
-        frame_align_union = frame_ats.filter(((aName == "CA") & (rId.is_in(ss_residues))) | dna_align_pred)
-
-        # align reference by first frame nucleic P
-        alignment = calc_alignment(frame_ats.filter(probe).toCoords, ref_ats.filter(probe).toCoords)
-        ref_ats.transform(alignment)
 
         ats_ref = []
         ats_frame = []
         for cid in sorted(protein_and_dna_chains):
-            ats_ref.append(ref_ats.filter(cName == cid))
+            ats_ref.append(ref_pbc_ats.filter(cName == cid))
             ats_frame.append(frame_ats.filter(cName == cid))
 
+    # unwrapping
     # align all reference atoms by one of DNA chains
-    ref_ats.transform(ref_probe.alignment_to(frame_probe))
+    ref_pbc_ats.transform(ref_probe.alignment_to(frame_probe))
 
     # cycle through chains and check first atom displacement
-    shift_finder.scale_lattice_by(scaling_factors[frame.index])
+    shift_finder.scale_lattice_by(scaling_factors[int(frame.index / stride)])
     for i, cid in enumerate(sorted(protein_and_dna_chains)):
         dif, shift = shift_finder.find_best_shift(ats_ref[i].geom_center(),
                                                   ats_frame[i].geom_center())
         ats_frame[i].transform(Translation3d(shift))
-    shift_finder.scale_lattice_by(1.0/scaling_factors[frame.index])
+    shift_finder.scale_lattice_by(1.0 / scaling_factors[int(frame.index / stride)])
+
+    # align nucleosome in frame by reference 3LZ0 structure using sec.str. CA atoms
+    frame_ats.transform(frame_align_ca_ss.alignment_to(ref_align_ca_ss))
 
     # calculate RMSD
-    # frame_align_protein.align_to(ref_align_protein)
-    # rmsd_ref_align_protein[int(frame.index / stride)] = frame_align_protein.rmsd(ref_align_protein)
-    alignment = calc_alignment(ref_align_protein.toCoords, frame_align_protein.toCoords)
-    rmsd_ref_align_protein[int(frame.index / stride)] = calc_rmsd(ref_align_protein.toCoords, frame_align_protein.toCoords, alignment)
-
-    # frame_align_ca.align_to(ref_align_ca)
-    # rmsd_ref_align_ca[int(frame.index / stride)] = frame_align_ca.rmsd(ref_align_ca)
-    alignment = calc_alignment(ref_align_ca.toCoords, frame_align_ca.toCoords)
-    rmsd_ref_align_ca[int(frame.index / stride)] = calc_rmsd(ref_align_ca.toCoords, frame_align_ca.toCoords, alignment)
-
-    # frame_align_ca_ss.align_to(ref_align_ca_ss)
-    # rmsd_ref_align_ca_ss[int(frame.index / stride)] = frame_align_ca_ss.rmsd(ref_align_ca_ss)
-    alignment = calc_alignment(ref_align_ca_ss.toCoords, frame_align_ca_ss.toCoords)
-    rmsd_ref_align_ca_ss[int(frame.index / stride)] = calc_rmsd(ref_align_ca_ss.toCoords, frame_align_ca_ss.toCoords, alignment)
-
-    # frame_align_dna.align_to(ref_align_dna)
-    # rmsd_ref_align_dna[int(frame.index / stride)] = frame_align_dna.rmsd(ref_align_dna)
-    alignment = calc_alignment(ref_align_dna.toCoords, frame_align_dna.toCoords)
-    rmsd_ref_align_dna[int(frame.index / stride)] = calc_rmsd(ref_align_dna.toCoords, frame_align_dna.toCoords, alignment)
-
-    alignment = calc_alignment(ref_align_inner_turn.toCoords, frame_align_inner_turn.toCoords)
-    rmsd_ref_align_inner_turn[int(frame.index / stride)] = calc_rmsd(ref_align_inner_turn.toCoords,
-                                                                     frame_align_inner_turn.toCoords, alignment)
-
-    alignment = calc_alignment(ref_align_outer_turn.toCoords, frame_align_outer_turn.toCoords)
-    rmsd_ref_align_outer_turn[int(frame.index / stride)] = calc_rmsd(ref_align_outer_turn.toCoords,
-                                                                     frame_align_outer_turn.toCoords, alignment)
-
-    alignment = calc_alignment(ref_align_union.toCoords, frame_align_union.toCoords)
-    rmsd_ref_align_union[int(frame.index / stride)] = calc_rmsd(ref_align_union.toCoords,
-                                                                frame_align_union.toCoords, alignment)
+    rmsd_ref_align_ca_ss[int(frame.index / stride)] = calc_rmsd(ref_align_ca_ss.toCoords, frame_align_ca_ss.toCoords)
+    rmsd_ref_align_dna[int(frame.index / stride)] = calc_rmsd(ref_align_dna.toCoords, frame_align_dna.toCoords)
+    rmsd_ref_align_union[int(frame.index / stride)] = calc_rmsd(ref_align_union.toCoords, frame_align_union.toCoords)
+    rmsd_ref_align_inner_turn[int(frame.index / stride)] = calc_rmsd(ref_align_inner_turn.toCoords, frame_align_inner_turn.toCoords)
+    rmsd_ref_align_outer_turn[int(frame.index / stride)] = calc_rmsd(ref_align_outer_turn.toCoords, frame_align_outer_turn.toCoords)
 
 
-# write RMSD to file
+#
+#  write RMSD to file
+#
+header = ["time [ps]",
+          r"sec.str. C$\rm\alpha$ [A]",
+          "DNA [A]",
+          r"DNA & sec.str. C$\rm\alpha$ [A]",
+          "DNA inner turn [A]",
+          "DNA outer turn [A]"]
 time = np.linspace((first_dat_file - 1) * 1000 + stride, last_dat_file * 1000, int(traj.size / stride))
+rmsd = np.vstack((time,
+                  rmsd_ref_align_ca_ss,
+                  rmsd_ref_align_dna,
+                  rmsd_ref_align_union,
+                  rmsd_ref_align_inner_turn,
+                  rmsd_ref_align_outer_turn)).T
 np.savetxt(
-    fname="rmsd_ref_align_protein.csv",
-    X=np.vstack((time, rmsd_ref_align_protein)).T,
+    fname=rmsd_fnout,
+    X=rmsd,
     fmt="%.5f",
-    header="time[ps], rmsd_ref_align_protein[A]",
-    delimiter=","
-)
-np.savetxt(
-    fname="rmsd_ref_align_ca.csv",
-    X=np.vstack((time, rmsd_ref_align_ca)).T,
-    fmt="%.5f",
-    header="time[ps], rmsd_ref_align_ca[A]",
-    delimiter=","
-)
-np.savetxt(
-    fname="rmsd_ref_align_ca_ss.csv",
-    X=np.vstack((time, rmsd_ref_align_ca_ss)).T,
-    fmt="%.5f",
-    header="time[ps], rmsd_ref_align_ca_ss[A]",
-    delimiter=","
-)
-np.savetxt(
-    fname="rmsd_ref_align_dna.csv",
-    X=np.vstack((time, rmsd_ref_align_dna)).T,
-    fmt="%.5f",
-    header="time[ps], rmsd_ref_align_dna[A]",
-    delimiter=","
-)
-np.savetxt(
-    fname="rmsd_ref_align_inner_turn.csv",
-    X=np.vstack((time, rmsd_ref_align_inner_turn)).T,
-    fmt="%.5f",
-    header="time[ps], rmsd_ref_align_inner_turn[A]",
-    delimiter=","
-)
-np.savetxt(
-    fname="rmsd_ref_align_outer_turn.csv",
-    X=np.vstack((time, rmsd_ref_align_outer_turn)).T,
-    fmt="%.5f",
-    header="time[ps], rmsd_ref_align_outer_turn[A]",
-    delimiter=","
-)
-np.savetxt(
-    fname="rmsd_ref_align_union.csv",
-    X=np.vstack((time, rmsd_ref_align_union)).T,
-    fmt="%.5f",
-    header="time[ps], rmsd_ref_align_union[A]",
+    header=",".join(header),
     delimiter=","
 )
--- chromatin/RMSD/plot_rmsd.py
+++ chromatin/RMSD/plot_rmsd.py
@@ -2,37 +2,67 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
 
+#
+#  setup trajectory parameters
+#
+with open("input.json", "r") as f:
+    pars = json.load(f)
+first_dat_file = int(pars["first_dat_file"])
+last_dat_file = int(pars["last_dat_file"])
+stride = int(pars["stride"])
+reference_pdb = pars["reference_pdb"]
+rmsd_fn = pars["rmsd_fnout"]
+len_inner_turn = pars["len_inner_turn"]
+panel_labels = pars["panel_labels"]
+axis_limits = pars["axis_limits"]
+fig_fn = pars["fig_fn"]
+
+
+#
+#  read RMSD data
+#
+rmsd = np.loadtxt(rmsd_fn, skiprows=1, delimiter=",")
+with open(rmsd_fn, "r") as f:
+    header = f.readline()
+lab = header.split(",")
+
+
+#
+#  plot parameters
+#
 plt.rcParams.update({'font.size': 12})
-colors = ["blue", "red", "green", "magenta", "black"]
+colors = ["blue", "red", "black", "magenta", "green"]
+fig, axs = plt.subplots(1, 2, figsize=(32/2.54, 12/2.54), dpi=300)
 
 
-def cm2inch(*tupl):
-    inch = 2.54
-    if isinstance(tupl[0], tuple):
-        return tuple(i/inch for i in tupl[0])
-    else:
-        return tuple(i/inch for i in tupl)
+#
+#  plot
+#
+axs[0].plot(rmsd[:, 0]/1000, rmsd[:, 1], color=colors[0], linewidth=1.0, label=lab[1][:-4])
+axs[0].plot(rmsd[:, 0]/1000, rmsd[:, 2], color=colors[1], linewidth=1.0, label=lab[2][:-4])
+axs[0].plot(rmsd[:, 0]/1000, rmsd[:, 3], color=colors[2], linewidth=1.0, label=lab[3][:-4])
+axs[1].plot(rmsd[:, 0]/1000, rmsd[:, 4], color=colors[3], linewidth=1.0, label=lab[4][:-4])
+axs[1].plot(rmsd[:, 0]/1000, rmsd[:, 5], color=colors[4], linewidth=1.0, label=lab[5][:-4])
 
+axs[0].set(xlabel="Time, ns", ylabel=r"RMSD, ${\rm\AA}$")
+axs[1].set(xlabel="Time, ns", ylabel=r"RMSD, ${\rm\AA}$")
+axs[0].set_xlim(axis_limits[:2])
+axs[1].set_xlim(axis_limits[:2])
+axs[0].set_ylim(axis_limits[2:])
+axs[1].set_ylim(axis_limits[2:])
+axs[0].legend()
+axs[1].legend()
+axs[0].text(.03, .92, panel_labels[0],
+            fontsize=16,
+            horizontalalignment='left',
+            transform=axs[0].transAxes)
+axs[1].text(.03, .92, panel_labels[1],
+            fontsize=16,
+            horizontalalignment='left',
+            transform=axs[1].transAxes)
 
-# files = sorted(glob('rmsd_*.csv'))
-files = ["rmsd_ref_align_ca_ss.csv", "rmsd_ref_align_dna.csv",
-         "rmsd_ref_align_inner_turn.csv", "rmsd_ref_align_outer_turn.csv",
-         "rmsd_ref_align_union.csv"]
-lab = ["sec.str. CA", "DNA", "DNA inner turn", "DNA outer turn", "sec.str. CA & DNA"]
-plt.figure(figsize=cm2inch(16, 12), dpi=300)
-
-for i, fn in enumerate(files):
-    data = np.loadtxt(fn, skiprows=1, delimiter=",")
-    x = data[:, 0] / 1000
-    y = data[:, 1]
-    plt.plot(x, y, color=colors[i], linewidth=1.0, label=lab[i])
-
-plt.xlabel("Time, ns")
-plt.ylabel(r"RMSD, ${\rm\AA}$")
-plt.legend()
-plt.axis([-10, 1010, 0.9, 6.0])
-
-plt.savefig("RMSD.png", bbox_inches="tight")
-plt.savefig("RMSD.pdf", bbox_inches="tight")
+plt.savefig(fig_fn + ".png", bbox_inches="tight")
+plt.savefig(fig_fn + ".pdf", bbox_inches="tight")
